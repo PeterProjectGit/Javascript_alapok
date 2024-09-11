feaf7209b2b71842c002e8a22b280da543ce7eb5
antalpéterdániel <petya.antal@gmail.com>
2024-09-11 13:48:23 +0200

    a

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/////////////////////////////////////////66666 my thaughts getting 6, if sb kill me in my head
+/* 
+let let    
+let a = 23;
+
+let -> block scope-al rendelkezik
+var -> function scope--
+Állandó :
+const ->block scope
+
+elemi típusok:
+string:
+'', "", `string (template) literal ${változó}`
+number: 3, 3.12 parseInt(), Number(), parseFloat() NaN egy number 'érték'
+boolean
+undefined
+null -> vagy null a definiált értéke, vagy semmit adtak értéknek
+
+Operátorok:
++ (túlterhelt), *, -, /, %(modulus), //nincs!! = értékadó
+inkrementálás: ++
+dekrementálás: --
+
+Összehasonlító operátorok:
+>,<, <=, >=, ==, === typeosszehasonlítógyerek,
+
+logikai operátorok: negálás: !, and -> &&, or -> ||
+
+3+"4" -> "34"
+3*"4" -> 12
+
+A Js gyengén típusos nyelv: ==
+
+*/
+ 
